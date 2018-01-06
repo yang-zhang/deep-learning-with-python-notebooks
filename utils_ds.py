c2fbcee36733a29823693ba86d40152cf7c14f43
yang-zhang <zhangyangcolostate@gmail.com>
2018-01-06 11:02:40 -0500

    Add util

---
+++ utils_ds.py
@@ -0,0 +1,71 @@
+import os
+import numpy as np
+import glob
+import shutil
+from keras.preprocessing import image
+
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+
+def move_sample(dir_source, dir_destin, file_type, n):
+    """
+    :param dir_source:
+    :param dir_destin:
+    :param file_type:
+    :param n:
+    :return:
+    Example:
+        move_sample(dir_source=data_dir+'/preprocessed/train', dir_destin=data_dir+'/preprocessed/sample/train', file_type='jpg', 200)
+    """
+    g = glob.glob(dir_source + '/*' + file_type)
+    fs = [pth.split('/')[-1] for pth in g]
+    fs_shuffle = np.random.permutation(fs)
+    for i in range(n):
+        os.rename(dir_source + '/' + fs_shuffle[i], dir_destin + '/' + fs_shuffle[i])
+
+
+def copy_sample(dir_source, dir_destin, file_type, n):
+    """
+    :param dir_source:
+    :param dir_destin:
+    :param file_type:
+    :param n:
+    :return:
+    Example:
+        copy_sample(dir_source=data_dir+'/preprocessed/train', dir_destin=data_dir+'/preprocessed/sample/train', file_type='jpg', 200)
+    """
+    g = glob.glob(dir_source + '/*' + file_type)
+    fs = [pth.split('/')[-1] for pth in g]
+    fs_shuffle = np.random.permutation(fs)
+    for i in range(n):
+        shutil.copyfile(dir_source + '/' + fs_shuffle[i],
+                        dir_destin + '/' + fs_shuffle[i])
+
+
+def get_batches(dirname, gen=image.ImageDataGenerator(),
+                shuffle=True, batch_size=4, class_mode='categorical',
+                target_size=(224, 224)):
+    return gen.flow_from_directory(dirname,
+                                   target_size=target_size,
+                                   class_mode=class_mode,
+                                   shuffle=shuffle,
+                                   batch_size=batch_size)
+
+
+def plot_keras_model(model, show_shapes=True, show_layer_names=True):
+    return SVG(model_to_dot(model,
+                            show_shapes=show_shapes,
+                            show_layer_names=show_layer_names).create(
+                            prog='dot', format='svg'))
+
+
+def print_weights_shape(model):
+    for layer in model.layers:
+        print('layer:', layer.name)
+        print('input_shape:', layer.input_shape)
+        print('output_shape:', layer.output_shape)
+        print('shape of weights:')
+        weights = layer.get_weights()
+        for i in weights:
+            print(i.shape)
+            print('-' * 13)
